89c756a260d83349b2726718c9b6f1b590bf3bd7
Артём <slavasatane4434343@gmail.com>
2020-08-31 19:22:54 +0300

    Updates

---
+++ precache-manifest.d5217d0ee01f0a4e7d95547d7bfb22a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce2fc3d59ff138bdfbe460314741865",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/index.html"
+  },
+  {
+    "revision": "640f5c9305751c2eaf59",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/css/main.0a855367.chunk.css"
+  },
+  {
+    "revision": "8676aa6d60880cd8b986",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/js/2.f2058a78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/js/2.f2058a78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "640f5c9305751c2eaf59",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/js/main.910c53af.chunk.js"
+  },
+  {
+    "revision": "302ee4bb0670f1ea5581",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/js/runtime-main.8f4a27b4.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/media/Exo-Thin.44bde874.otf"
+  },
+  {
+    "revision": "c9086e61ca0d48c7ac041d2b152a2f39",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/media/Exo-Thin.c9086e61.eot"
+  },
+  {
+    "revision": "087f2a66661ad4bde52e3d08e3ef0c82",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/media/bg.087f2a66.png"
+  },
+  {
+    "revision": "e233614be4fb72ed0918a3d1197a72ed",
+    "url": "/Artyom2510/Artyom2510.github.io/cat-react/static/media/cat.e233614b.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Artyom2510/Artyom2510.github.io/cat-react/precache-manifest.d5217d0ee01f0a4e7d95547d7bfb22a2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Artyom2510/Artyom2510.github.io/cat-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
